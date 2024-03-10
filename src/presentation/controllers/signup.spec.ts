8f8e8485d3d66f399f0ca12db0b24435b34e9f4e
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2024-03-10 00:15:00 -0300

    test: ensure SignUpController returns error if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -41,4 +41,23 @@ describe('SignUp Controller', () => {
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new MissingParamError('email'))
   })
+  test('Should return error if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const response = sut.handle(httpRequest)
+
+    if (!response) {
+      throw new Error('Response is not defined')
+    }
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('password'))
+  })
 })
