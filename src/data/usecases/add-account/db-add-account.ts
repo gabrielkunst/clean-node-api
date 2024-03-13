7d35d3832ab658196c480e8a9b1d1e31a12c3f99
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2024-03-12 21:23:46 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,24 +1,25 @@
+import { type AddAccountRepository } from '@/data/protocols/add-account-reposltory'
 import { type Encrypter } from '@/data/protocols/encrypter'
 import { type AccountModel } from '@/domain/models/account'
-import { type AddAccountModel, type AddAccount } from '@/domain/usecases/add-account'
+import {
+  type AddAccountModel,
+  type AddAccount
+} from '@/domain/usecases/add-account'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    const hashedPassword = await this.encrypter.encrypt(account.password)
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
 
-    const createdAccount: AccountModel = {
-      email: 'valid_email@email.com',
-      name: 'valid_name',
-      id: 'valid_id',
-      password: hashedPassword
-    }
+    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
 
-    return createdAccount
+    return account
   }
 }
