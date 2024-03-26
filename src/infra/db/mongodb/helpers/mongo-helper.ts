304a3146ba869f97fff582059e710a2ec54a0aa5
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2024-03-25 21:32:33 -0300

    refactor: add env file

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,19 +3,19 @@ import { type Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: MongoClient,
 
-  async connect (url: string): Promise<void> {
+  async connect(url: string): Promise<void> {
     this.client = await MongoClient.connect(url)
   },
 
-  async disconnect (): Promise<void> {
+  async disconnect(): Promise<void> {
     await this.client.close()
   },
 
-  getCollection (name: string): Collection {
+  getCollection(name: string): Collection {
     return this.client.db().collection(name)
   },
 
-  map <T>(document: any): T {
+  map<T>(document: any): T {
     const { _id, ...documentWithoutId } = document
     return Object.assign({}, documentWithoutId, { id: _id })
   }
--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,21 @@
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { type Router } from 'express'
+import env from '../config/env'
 
 export const signupRoutes = (router: Router): void => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   router.post('/signup', (req, res) => {
     res.send()
   })
