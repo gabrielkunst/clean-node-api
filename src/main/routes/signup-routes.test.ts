4317cfe2dc2b1c6a63e833e29fc8fb09a56e7444
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2024-03-26 20:50:42 -0300

    refactor: move testing mongo db connection to test file

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
-import request from 'supertest'
 import app from '../config/app'
+import env from '../config/env'
+import request from 'supertest'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return 200', async () => {
     await request(app).post('/api/v1/signup').send().expect(200)
   })
